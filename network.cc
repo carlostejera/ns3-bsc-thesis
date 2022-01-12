816e6fe4f6875b31e3e0dff948d9ad1fa39f6adb
carlosandrestejera <carlos.tejera@stud.unibas.ch>
2022-01-12 08:38:26 +0100

    New topologies added

--- network.cc
+++ network.cc
@@ -4,6 +4,7 @@
 #include "Manager.h"
 #include "../../build/ns3/node-container.h"
 #include "../../build/ns3/point-to-point-helper.h"
+#include "../../build/ns3/animation-interface.h"
 
 using namespace ns3;
 using namespace std;
@@ -54,29 +55,30 @@ void addApplicationToNodes(Ptr<T>* apps, NodeContainer nodes, uint32_t beginFrom
 
 
     for (uint32_t i = 0; i < nodes.GetN(); i++) {
-        auto privKey = readFile(files.at(c) + "/priv.txt", true);
-        auto pubKey = readFile(files.at(c) + "/pub.txt", false);
-        apps[i] = Create<T>(to_string(beginFrom + i), gossipInterval, privKey);
+//        auto privKey = readFile(files.at(c) + "/priv.txt", true);
+//        auto pubKey = readFile(files.at(c) + "/pub.txt", false);
+        apps[i] = Create<T>(to_string(beginFrom + i), gossipInterval, "");
         nodes.Get(i)->AddApplication(apps[i]);
         c++;
     }
 }
 
-void confNodes(AnimationInterface& anim,
-                 NodeContainer& nodeContainer,
-                 string path,
-                 string desc,
-                 int x,
-                 int y,
-                 int xDistance = 0,
-                 int yDistance = 0) {
+void confNodes(AnimationInterface &anim,
+               NodeContainer &nodeContainer,
+               string path,
+               string desc,
+               int baseNum,
+               int x,
+               int y,
+               int xDistance = 0,
+               int yDistance = 0) {
     Ptr<Node> node;
     for (uint32_t i = 0; i < nodeContainer.GetN(); i++) {
         node = nodeContainer.Get(i);
         anim.SetConstantPosition(node, x, y);  //set position of nodes in the animaiton
         anim.UpdateNodeSize(node->GetId(), 10, 10);
         anim.UpdateNodeImage(node->GetId(), anim.AddResource(path));
-        anim.UpdateNodeDescription(node->GetId(), desc + ":" + to_string(i));
+        anim.UpdateNodeDescription(node->GetId(), desc + ":" + to_string(baseNum + i));
         x += xDistance;
         y += yDistance;
     }
@@ -90,75 +92,80 @@ void disablePacketLoss(Ptr<ErrorModel> em) {
     em->Disable();
 }
 
-void lineTopologyInterface(Ptr<User> *user_apps,
-                           Ptr<EthSwitch> *switch_apps,
-                           Ptr<Manager> *manager_apps,
+void lineTopologyInterface(AnimationInterface &anim,
                            NodeContainer &user_nodes,
                            NodeContainer &switch_nodes,
                            NodeContainer &manager_nodes,
-                           PointToPointHelper &p2p,
-                           Ptr<ErrorModel> em)
+                           PointToPointHelper &p2p)
                             {
-    p2p.EnablePcapAll ("myNetworkPcaps");
-    ////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////
-    double SWITCH_JOINING = 1;
-    double USER_JOINING = 2;
-    double ENABLE_ERROR_RATE = 2.5;
-    double USER0_SUBSCRIBING_USER1 = 5;
-    double USER1_PUSHING_START = 5;
-//    double STOP_PUSHING = 15;
-    double STOP_SIMULATION = 10;
-    ////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////
-
     // CONSTELLATION
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    AnimationInterface anim("hat-topology.xml");
     anim.EnablePacketMetadata(true);
 
-    int distance = 100 / ((switch_nodes.GetN() * user_nodes.GetN()) - 1);
+    int distance = 100 / (switch_nodes.GetN() + 1);
 
-    confNodes(anim, user_nodes, userPath, "User" + to_string(0), 0, 50, 100);
-    confNodes(anim, switch_nodes, switchPath, "Switch" + to_string(ethId), distance, 50, distance);
-    confNodes(anim, manager_nodes, managerPath, "Manager" + to_string(managerId), 50, 0);
+                                confNodes(anim, user_nodes, userPath, "User", 0, distance, 75, distance);
+                                confNodes(anim, switch_nodes, switchPath, "Switch", ethId, distance, 50, distance);
+                                confNodes(anim, manager_nodes, managerPath, "Manager", managerId, 50, 0);
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    }
 
-
-    // SCHEDULE
+void starTopologyInterface(AnimationInterface &anim,
+                           NodeContainer &user_nodes,
+                           NodeContainer &switch_nodes,
+                           NodeContainer &manager_nodes,
+                           PointToPointHelper &p2p)
+{
+    // CONSTELLATION
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    for (uint32_t i = 0; i < switch_nodes.GetN(); i++) {
-        switch_apps[i]->SetStartTime(Seconds(SWITCH_JOINING));
-    }
-    for (uint32_t i = 0; i < user_nodes.GetN(); i++) {
-        user_apps[i]->SetStartTime(Seconds(USER_JOINING));
-    }
-    Simulator::Schedule(Seconds(ENABLE_ERROR_RATE), &enablePacketLoss, em);
+    anim.EnablePacketMetadata(true);
 
-    Simulator::Schedule(Seconds(USER0_SUBSCRIBING_USER1), &User::subscribe, user_apps[0], "user:1");
+    NodeContainer userNC1(user_nodes.Get(0), user_nodes.Get(1));
+    NodeContainer userNC2(user_nodes.Get(2));
 
-    int packetNum = 10;
-    int i = 0;
-      while (packetNum > i) {
-        Simulator::Schedule(Seconds(USER1_PUSHING_START), &User::pushLogToSwitch, user_apps[1]);
-        USER1_PUSHING_START += 0.01;
-        i++;
-    }
-//    Simulator::Schedule(Seconds(6), &User::unsubscribe, user_apps[0], "user:1");
+    NodeContainer switchNC1(switch_nodes.Get(0), switch_nodes.Get(1));
+    NodeContainer switchNC2(switch_nodes.Get(2), switch_nodes.Get(3));
+    NodeContainer switchNC3(switch_nodes.Get(4));
+    confNodes(anim, userNC1, userPath, "User", 0, 0, 100, 100);
+    confNodes(anim, userNC2, userPath, "User", 0+ 2, 100, 0, 100);
+
+    confNodes(anim, switchNC1, switchPath, "Switch", ethId, 25, 25, 50);
+    confNodes(anim, switchNC2, switchPath, "Switch", ethId + 2, 25, 75, 50);
+    confNodes(anim, switchNC3, switchPath, "Switch", ethId + 4, 50, 50, 0);
+
+    confNodes(anim, manager_nodes, managerPath, "Manager", managerId, 50, 0);
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    Simulator::Stop(Seconds(STOP_SIMULATION));
-    Simulator::Run(); //run simulation
-    Simulator::Destroy(); //end simulation
+}
 
-    }
+void diamondTopologyInterface(AnimationInterface &anim,
+                           NodeContainer &user_nodes,
+                           NodeContainer &switch_nodes,
+                           NodeContainer &manager_nodes,
+                           PointToPointHelper &p2p)
+{
+    // CONSTELLATION
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    anim.EnablePacketMetadata(true);
+    int distance = 100 / (switch_nodes.GetN() + 1);
+
+
+    NodeContainer switchNC1(switch_nodes.Get(0), switch_nodes.Get(1), switch_nodes.Get(2));
+    NodeContainer switchNC3(switch_nodes.Get(3));
+    confNodes(anim, user_nodes, userPath, "User", 0, 0, 50, 100);
+
+    confNodes(anim, switchNC1, switchPath, "Switch", ethId, distance, 50, distance);
+    confNodes(anim, switchNC3, switchPath, "Switch", ethId + 4, 50, 75, 0);
+
+    confNodes(anim, manager_nodes, managerPath, "Manager", managerId, 50, 0);
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+}
 
 void nodesSetup(const uint32_t userNumbers,
                 const uint32_t switchNumbers,
                 const uint32_t managerNumbers,
                 const double errorRate,
-                const double gossipInterval)
+                const double gossipInterval,
+                std::string topology)
                   {
     errorSettings(errorRate);
 
@@ -179,17 +186,47 @@ void nodesSetup(const uint32_t userNumbers,
     PointToPointHelper p2p;
     p2p.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
     p2p.SetChannelAttribute("Delay", StringValue("2ms"));
-    
-    // Connect switches to switches and to the manager /////////////////////////////////////////////////////////////////
-    for (uint32_t i = 0; i < switchNumbers - 1; i++) {
-        switchToSwitchContainer.Add(p2p.Install(switch_nodes.Get(i), switch_nodes.Get(i + 1)));
-        mangerToSwitchContainer.Add(p2p.Install(switch_nodes.Get(i), manager_nodes.Get(0)));
-    }
-    mangerToSwitchContainer.Add(p2p.Install(switch_nodes.Get(switchNumbers - 1), manager_nodes.Get(0)));
 
-    //Connect users to switches ////////////////////////////////////////////////////////////////////////////////////////
-    userToSwitchContainer.Add(p2p.Install(user_nodes.Get(0), switch_nodes.Get(0)));
-    userToSwitchContainer.Add(p2p.Install(user_nodes.Get(1), switch_nodes.Get(switchNumbers - 1)));
+  if (topology == "line") {
+      // Connect switches to switches and to the manager /////////////////////////////////////////////////////////////////
+      for (uint32_t i = 0; i < switchNumbers - 1; i++) {
+          switchToSwitchContainer.Add(p2p.Install(switch_nodes.Get(i), switch_nodes.Get(i + 1)));
+          mangerToSwitchContainer.Add(p2p.Install(switch_nodes.Get(i), manager_nodes.Get(0)));
+      }
+      mangerToSwitchContainer.Add(p2p.Install(switch_nodes.Get(switchNumbers - 1), manager_nodes.Get(0)));
+
+      //Connect users to switches ////////////////////////////////////////////////////////////////////////////////////////
+      userToSwitchContainer.Add(p2p.Install(user_nodes.Get(0), switch_nodes.Get(0)));
+      userToSwitchContainer.Add(p2p.Install(user_nodes.Get(1), switch_nodes.Get( 1)));
+      userToSwitchContainer.Add(p2p.Install(user_nodes.Get(2), switch_nodes.Get(2)));
+  } else if (topology == "star") {
+      // Connect switches to switches and to the manager /////////////////////////////////////////////////////////////////
+      for (uint32_t i = 0; i < switchNumbers - 1; i++) {
+          switchToSwitchContainer.Add(p2p.Install(switch_nodes.Get(i), switch_nodes.Get(switchNumbers - 1)));
+          mangerToSwitchContainer.Add(p2p.Install(switch_nodes.Get(i), manager_nodes.Get(0)));
+      }
+      mangerToSwitchContainer.Add(p2p.Install(switch_nodes.Get(switchNumbers - 1), manager_nodes.Get(0)));
+
+      //Connect users to switches ////////////////////////////////////////////////////////////////////////////////////////
+      userToSwitchContainer.Add(p2p.Install(user_nodes.Get(0), switch_nodes.Get(2)));
+      userToSwitchContainer.Add(p2p.Install(user_nodes.Get(1), switch_nodes.Get( 3)));
+      userToSwitchContainer.Add(p2p.Install(user_nodes.Get(2), switch_nodes.Get(1)));
+  }
+  else if (topology == "dia") {
+      for (uint32_t i = 0; i < switchNumbers - 2; i++) {
+          switchToSwitchContainer.Add(p2p.Install(switch_nodes.Get(i), switch_nodes.Get(i + 1)));
+          mangerToSwitchContainer.Add(p2p.Install(switch_nodes.Get(i), manager_nodes.Get(0)));
+      }
+      mangerToSwitchContainer.Add(p2p.Install(switch_nodes.Get(switchNumbers - 2), manager_nodes.Get(0)));
+      mangerToSwitchContainer.Add(p2p.Install(switch_nodes.Get(switchNumbers - 1), manager_nodes.Get(0)));
+      mangerToSwitchContainer.Add(p2p.Install(switch_nodes.Get(switchNumbers - 1), switch_nodes.Get(0)));
+      mangerToSwitchContainer.Add(p2p.Install(switch_nodes.Get(switchNumbers - 1), switch_nodes.Get(2)));
+      userToSwitchContainer.Add(p2p.Install(user_nodes.Get(0), switch_nodes.Get(0)));
+      userToSwitchContainer.Add(p2p.Install(user_nodes.Get(1), switch_nodes.Get(2)));
+
+  }
+    
+
 
     // Add the applications to the devices /////////////////////////////////////////////////////////////////////////////
     Ptr <User> user_apps[userNumbers];
@@ -215,14 +252,76 @@ void nodesSetup(const uint32_t userNumbers,
         mangerToSwitchContainer.Get(i)->SetAttribute("ReceiveErrorModel", PointerValue(em));
     }*/
     // Add error model for user to switch links
-    for (uint32_t i = 0; i < userToSwitchContainer.GetN(); i++) {
-//        userToSwitchContainer.Get(i)->SetAttribute("ReceiveErrorModel", PointerValue(em));
+/*    for (uint32_t i = 0; i < userToSwitchContainer.GetN(); i++) {
+        userToSwitchContainer.Get(i)->SetAttribute("ReceiveErrorModel", PointerValue(em));
+    }*/
+
+
+//    p2p.EnablePcapAll ("myNetworkPcaps");
+    ////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////
+    double SWITCH_JOINING = 1;
+    double USER_JOINING = 2;
+    double ENABLE_ERROR_RATE = 2.5;
+    double USER0_SUBSCRIBING_USER1 = 5;
+    double USER1_PUSHING_START = 5;
+//    double STOP_PUSHING = 15;
+    double STOP_SIMULATION = 10;
+    ////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////
+
+    std::string filename = "";
+    if (topology == "line") {
+        filename = "hat-topology.xml";
+    } else if (topology == "star") {
+        filename = "star-topology.xml";
+    }
+    else if (topology == "dia") {
+        filename = "dia-topology.xml";
+    }
+    AnimationInterface anim(filename);
+
+    if (topology == "line") {
+        lineTopologyInterface(anim, user_nodes, switch_nodes, manager_nodes, p2p);
+    } else if (topology == "star") {
+        starTopologyInterface(anim, user_nodes, switch_nodes, manager_nodes, p2p);
+
+    }
+    else if (topology == "dia") {
+        diamondTopologyInterface(anim, user_nodes, switch_nodes, manager_nodes, p2p);
+
     }
 
-    lineTopologyInterface(user_apps, switch_apps, manager_apps, user_nodes, switch_nodes, manager_nodes, p2p, em);
+    // SCHEDULE
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    for (uint32_t i = 0; i < switch_nodes.GetN(); i++) {
+        // Starting sending joins
+        switch_apps[i]->SetStartTime(Seconds(SWITCH_JOINING));
+    }
+    for (uint32_t i = 0; i < user_nodes.GetN(); i++) {
+        // Starting sending joins
+        user_apps[i]->SetStartTime(Seconds(USER_JOINING));
+    }
+    Simulator::Schedule(Seconds(ENABLE_ERROR_RATE), &enablePacketLoss, em);
+    Simulator::Schedule(Seconds(USER0_SUBSCRIBING_USER1), &User::subscribe, user_apps[0], "user:1");
+
+    int packetNum = 10;
+    int i = 0;
+    while (packetNum > i) {
+        Simulator::Schedule(Seconds(USER1_PUSHING_START), &User::pushLogToSwitch, user_apps[1]);
+        USER1_PUSHING_START += 0.01;
+        i++;
+    }
+//    Simulator::Schedule(Seconds(6), &User::unsubscribe, user_apps[0], "user:1");
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    Simulator::Stop(Seconds(STOP_SIMULATION));
+    Simulator::Run(); //run simulation
+    Simulator::Destroy(); //end simulation
 
 }
-
+/*
 void p2pTopology(const double errorRate) {
     errorSettings(errorRate);
 
@@ -275,6 +374,7 @@ void p2pTopology(const double errorRate) {
     Simulator::Destroy(); //end simulation
 
 }
+*/
 
 
 int main(int argc, char *argv[]) {
@@ -291,11 +391,15 @@ int main(int argc, char *argv[]) {
     Time::SetResolution(Time::NS);
     Packet::EnablePrinting();
     Packet::EnableChecking();
-
     if (topology == "line") {
-        nodesSetup(2, 2, 1, errorRate, gossip);
-    } else if (topology == "p2p") {
-        p2pTopology(0);
+        nodesSetup(3, 3, 1, errorRate, gossip, topology);
+    } else if (topology == "star") {
+        nodesSetup(3, 5, 1, errorRate, gossip, topology);
     }
+    else if (topology == "dia") {
+        nodesSetup(2, 4, 1, errorRate, gossip, topology);
+    }
+
+
     return 0;
 }
